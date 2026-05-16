cc2fc489b26917c81f4b2ceff8ae60fab19c0e6b
Mahbub101 <mahbubsolo101@gmail.com>
2026-05-16 15:58:18 +0600

    feat: add base model with db connection

---
+++ delivery_manager/models/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/../config/db.php';
+
+class BaseModel {
+    protected $conn;
+
+    public function __construct() {
+        $this->conn = getDB();
+    }
+
+    public function __destruct() {
+        $this->conn->close();
+    }
+}
