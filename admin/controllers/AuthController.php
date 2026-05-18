65be1db3b819e9bcb4835fa5a267b274ae93501f
NOMAN-hash-sudo <alnoman4720@gmail.com>
2026-05-18 15:08:37 +0600

    fix: update AuthController

--- admin/controllers/AuthController.php
+++ admin/controllers/AuthController.php
@@ -2,7 +2,7 @@
 session_start();
 require_once '../config/db.php';
 require_once '../models/AdminModel.php';
-// GJJGJGJJGJHgd
+// GJJGJGJJGJfgHgd
 $model  = new AdminModel($conn);
 $action = $_GET['action'] ?? '';
 
